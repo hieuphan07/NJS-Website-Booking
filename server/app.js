d1de70a0eeff0dbce1b81a54cc9ba81019418afa
Hieu Phan <hieuphan07@gmail.com>
2023-12-22 15:44:55 +0700

    first commit

---
+++ server/app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const serverPort = 5500;
+
+app.get('/', (req, res, next) => {
+	res.send('Hello from Node JS');
+});
+
+app.listen(serverPort, () => {
+	console.log(`Server listening on port ${serverPort}`);
+});
