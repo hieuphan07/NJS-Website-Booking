273de04f012458121e26317f4f1f42b5013e4061
Hieu Phan <hieuphan07@gmail.com>
2024-01-08 18:29:23 +0700

    Loading hotels for search page

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import RootLayout, { loader as tokenLoader } from './pages/Root';
+import RootLayout, { loader as coupleLoader } from './pages/Root';
 import Home from './pages/Home';
 import Detail from './pages/Detail';
 import Search from './pages/Search';
@@ -7,7 +7,6 @@ import Login from './pages/Login';
 import Signup from './pages/Signup';
 import Transaction from './pages/Transaction';
 import Error from './pages/Error';
-import { loader as hotelLoader } from '../src/components/features/UI/Features';
 import { action as authManipulateAction } from '../src/components/auth-action/auth-action';
 
 const router = createBrowserRouter([
@@ -15,9 +14,10 @@ const router = createBrowserRouter([
 		path: '/',
 		errorElement: <Error />,
 		element: <RootLayout />,
-		loader: tokenLoader,
+		id: 'root',
+		loader: coupleLoader,
 		children: [
-			{ index: true, element: <Home />, loader: hotelLoader },
+			{ index: true, element: <Home /> },
 			{ path: 'login', element: <Login />, action: authManipulateAction },
 			{ path: 'signup', element: <Signup />, action: authManipulateAction },
 			{ path: 'search', element: <Search /> },
--- client/src/components/features/UI/Features.jsx
+++ client/src/components/features/UI/Features.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { useLoaderData } from 'react-router-dom';
 
 import City from '../city/City';
 import Type from '../type/Type';
@@ -8,29 +7,15 @@ import HotelList from '../hotelList/HotelList';
 import './Features.css';
 
 const Features = () => {
-	const data = useLoaderData();
-
 	return (
 		<div className='features'>
 			<div className='features__container'>
 				<City />
 				<Type />
-				<HotelList data={data} />
+				<HotelList />
 			</div>
 		</div>
 	);
 };
 
 export default Features;
-
-export async function loader() {
-	try {
-		const response = await fetch('http://localhost:5500/hotels');
-		if (!response.ok) {
-			throw new Error(`${response.status} ${response.statusText}`);
-		}
-		return response;
-	} catch (err) {
-		throw new Error(err);
-	}
-}
