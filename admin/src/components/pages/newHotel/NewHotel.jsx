c0dea4d551b54e4d446512709e556a876d4d40f4
Hieu Phan <hieuphan07@gmail.com>
2024-04-04 18:13:06 +0700

    added useForm, applied for hotel name first

--- admin/src/components/pages/newHotel/NewHotel.jsx
+++ admin/src/components/pages/newHotel/NewHotel.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
 
 import './NewHotel.css';
 
@@ -7,56 +8,129 @@ const NewHotel = () => {
 		{ label: 'Yes', value: true },
 		{ label: 'No', value: false },
 	];
+
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm({
+		defaultValues: {
+			name: 'Four Points',
+		},
+	});
+
+	const onSubmit = (data) => {
+		console.log(data);
+	};
+
+	console.log(errors);
+
 	return (
 		<div className='hotelForm'>
-			<div className='title'>Add New Product</div>
-			<form>
+			<div className='title'>
+				<h1>Add New Product</h1>
+			</div>
+			<form onSubmit={handleSubmit(onSubmit)}>
 				<div className='top'>
 					<div className='left'>
+						{/* Hotel name */}
 						<label htmlFor='name'>Name</label>
-						<input id='name' type='text' placeholder='My Hotel' />
+						<input
+							id='name'
+							type='text'
+							placeholder='My Hotel'
+							{...register('name', { pattern: /^[A-Za-z\s]+$/i })}
+						/>
+						<p>{errors.name?.message}</p>
+						{/* City */}
 						<label htmlFor='city'>City</label>
-						<input id='city' type='text' placeholder='Da Nang' />
+						<input
+							id='city'
+							type='text'
+							placeholder='Da Nang'
+							{...register('city')}
+						/>
+						{/* Distance */}
 						<label htmlFor='distance'>Distance From City Center</label>
-						<input id='distance' type='text' placeholder='500' />
+						<input
+							id='distance'
+							type='number'
+							placeholder='500'
+							{...register('distance')}
+						/>
+						{/* Description */}
 						<label htmlFor='description'>Description</label>
-						<input id='description' type='text' placeholder='Description' />
+						<input
+							id='description'
+							type='text'
+							placeholder='Description'
+							{...register('description')}
+						/>
+						{/* Images */}
 						<label htmlFor='images'>Images</label>
 						<input
-							id='images'
 							type='text'
 							style={{ border: '1px solid #333' }}
+							{...register('images')}
 						/>
 					</div>
 					<div className='right'>
+						{/* Type of accommodation */}
 						<label htmlFor='type'>Type</label>
-						<input id='type' type='text' placeholder='Hotel' />
+						<input
+							id='type'
+							type='text'
+							placeholder='Hotel'
+							{...register('type')}
+						/>
+						{/* Address */}
 						<label htmlFor='address'>Address</label>
 						<input
 							id='address'
 							type='text'
 							placeholder='Vo Nguyen Giap Street, Son Tra District'
+							{...register('address')}
 						/>
+						{/* Title */}
 						<label htmlFor='title'>Title</label>
-						<input id='title' type='text' placeholder='Title' />
+						<input
+							id='title'
+							type='text'
+							placeholder='Title'
+							{...register('title')}
+						/>
+						{/* Price */}
 						<label htmlFor='price'>Price</label>
-						<input id='price' type='text' placeholder='Price' />
+						<input
+							id='price'
+							type='number'
+							placeholder='Price'
+							{...register('price')}
+						/>
+						{/* Featured */}
 						<label htmlFor='featured'>Featured</label>
 						<select id='featured' style={{ width: '50px', height: '30px' }}>
 							{features.map((option) => (
-								<option value={option.value}>{option.label}</option>
+								<option value={option.value} key={option.label}>
+									{option.label}
+								</option>
 							))}
 						</select>
 					</div>
 				</div>
 				<div className='center'>
+					{/* Rooms */}
 					<label htmlFor='rooms'>Rooms</label>
-					<textarea id='rooms' rows='5'>
-						Room 1, Room 2
-					</textarea>
+					<select id='rooms' multiple>
+						<option value='Room 1'>Room 1</option>
+						<option value='Room 2'>Room 2</option>
+						<option value='Room 3'>Room 3</option>
+					</select>
 				</div>
 				<div className='bottom'>
-					<button className='btn'>Send</button>
+					<button className='btn' type='submit'>
+						Send
+					</button>
 				</div>
 			</form>
 		</div>
