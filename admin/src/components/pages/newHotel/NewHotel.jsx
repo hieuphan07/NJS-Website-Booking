336374c320690f80e25b60a296033b7d680f8a16
Hieu Phan <hieuphan07@gmail.com>
2024-04-05 10:54:47 +0700

    added form validation for new hotel form

--- admin/src/components/pages/newHotel/NewHotel.jsx
+++ admin/src/components/pages/newHotel/NewHotel.jsx
@@ -15,7 +15,17 @@ const NewHotel = () => {
 		formState: { errors },
 	} = useForm({
 		defaultValues: {
-			name: 'Four Points',
+			name: '',
+			city: '',
+			distance: '',
+			description: '',
+			images: [],
+			type: '',
+			address: '',
+			title: '',
+			price: '',
+			featured: true,
+			rooms: [],
 		},
 	});
 
@@ -39,33 +49,38 @@ const NewHotel = () => {
 							id='name'
 							type='text'
 							placeholder='My Hotel'
-							{...register('name', { pattern: /^[A-Za-z\s]+$/i })}
+							{...register('name', { required: 'Please enter hotel name' })}
 						/>
-						<p>{errors.name?.message}</p>
+						<p className='errors-msg'>{errors.name?.message}</p>
 						{/* City */}
 						<label htmlFor='city'>City</label>
 						<input
 							id='city'
 							type='text'
 							placeholder='Da Nang'
-							{...register('city')}
+							{...register('city', { required: 'Please enter city' })}
 						/>
+						<p className='errors-msg'>{errors.city?.message}</p>
 						{/* Distance */}
 						<label htmlFor='distance'>Distance From City Center</label>
 						<input
 							id='distance'
 							type='number'
 							placeholder='500'
-							{...register('distance')}
+							{...register('distance', { required: 'Please enter distance' })}
 						/>
+						<p className='errors-msg'>{errors.distance?.message}</p>
 						{/* Description */}
 						<label htmlFor='description'>Description</label>
 						<input
 							id='description'
 							type='text'
 							placeholder='Description'
-							{...register('description')}
+							{...register('description', {
+								required: 'Please enter description',
+							})}
 						/>
+						<p className='errors-msg'>{errors.description?.message}</p>
 						{/* Images */}
 						<label htmlFor='images'>Images</label>
 						<input
@@ -75,38 +90,42 @@ const NewHotel = () => {
 						/>
 					</div>
 					<div className='right'>
-						{/* Type of accommodation */}
+						{/* Type */}
 						<label htmlFor='type'>Type</label>
 						<input
 							id='type'
 							type='text'
 							placeholder='Hotel'
-							{...register('type')}
+							{...register('type', { required: 'Please enter type' })}
 						/>
+						<p className='errors-msg'>{errors.type?.message}</p>
 						{/* Address */}
 						<label htmlFor='address'>Address</label>
 						<input
 							id='address'
 							type='text'
 							placeholder='Vo Nguyen Giap Street, Son Tra District'
-							{...register('address')}
+							{...register('address', { required: 'Please enter address' })}
 						/>
+						<p className='errors-msg'>{errors.address?.message}</p>
 						{/* Title */}
 						<label htmlFor='title'>Title</label>
 						<input
 							id='title'
 							type='text'
 							placeholder='Title'
-							{...register('title')}
+							{...register('title', { required: 'Please enter title' })}
 						/>
+						<p className='errors-msg'>{errors.title?.message}</p>
 						{/* Price */}
 						<label htmlFor='price'>Price</label>
 						<input
 							id='price'
 							type='number'
 							placeholder='Price'
-							{...register('price')}
+							{...register('price', { required: 'Please enter price' })}
 						/>
+						<p className='errors-msg'>{errors.price?.message}</p>
 						{/* Featured */}
 						<label htmlFor='featured'>Featured</label>
 						<select id='featured' style={{ width: '50px', height: '30px' }}>
@@ -121,7 +140,7 @@ const NewHotel = () => {
 				<div className='center'>
 					{/* Rooms */}
 					<label htmlFor='rooms'>Rooms</label>
-					<select id='rooms' multiple>
+					<select id='rooms' multiple {...register('rooms')}>
 						<option value='Room 1'>Room 1</option>
 						<option value='Room 2'>Room 2</option>
 						<option value='Room 3'>Room 3</option>
