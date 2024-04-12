4ea7bfd83ccfd28af5b5e3d78950617b781c7749
Hieu Phan <hieuphan07@gmail.com>
2024-04-12 18:28:34 +0700

    admin: modified to render hotel data by using useForm Hook

---
+++ admin/src/components/pages/hotelDetail/HotelDetail.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import NewHotel from '../newHotel/NewHotel';
+
+import './HotelDetail.css';
+
+const HotelDetail = () => {
+	return (
+		<div>
+			<NewHotel />
+		</div>
+	);
+};
+
+export default HotelDetail;
